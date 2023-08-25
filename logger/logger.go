e1f3e5f2543fa28c02d7e8a87d2ae2ff71dcf384
Sergey Ryazanov <majo78@mail.ru>
2023-08-25 04:47:41 +0300

    build(logger): Add pacage logger

---
+++ logger/logger.go
@@ -0,0 +1,30 @@
+package logger
+
+import (
+	"log"
+	"os"
+)
+
+func createLogsDirectory() error {
+	err := os.MkdirAll("logs", 0755)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func openLogFile() (*os.File, error) {
+	logFile, err := os.OpenFile("logs/log.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return nil, err
+	}
+	return logFile, nil
+}
+
+func setLogger(logFile *os.File) {
+	log.SetOutput(logFile)
+}
+
+func logLine() {
+	log.Println("---------------------------------------------------")
+}
